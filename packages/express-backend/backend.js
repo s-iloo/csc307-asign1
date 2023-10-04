65ddf17ae6ccab7da13eda57b02fd7c5bcf8b30c
iloo <igloo405@gmail.com>
2023-10-03 19:17:48 -0700

    all done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,13 +35,51 @@ const users = {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+const deleteUser = (id) => {
+  return users["users_list"].pop((user) => user["id"] === id);
+};
+const findUserByJobAndName = (job, name) => {
+  return users["users_list"].filter(
+    (user) => user["job"] === job && user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+  if (name != undefined && job != undefined) {
+    let result = findUserByJobAndName(job, name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let deleted = deleteUser(id);
+  if (deleted === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(deleted);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -52,6 +90,22 @@ app.get("/users", (req, res) => {
   } else {
     res.send(users);
   }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
