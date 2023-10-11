5f309892c756e0121a3576d1250c7749a14432d0
iloo <igloo405@gmail.com>
2023-10-11 13:14:01 -0700

    test cases

---
+++ packages/jest-unit-testing/module.test.js
@@ -0,0 +1,31 @@
+import mut from "./module.js";
+
+test("Testing sum -- success", () => {
+  const expected = 30;
+  const got = mut.sum(12, 18);
+  expect(got).toBe(expected);
+});
+
+test("Testing div -- success", () => {
+  const expected = 30;
+  const got = mut.div(60, 2);
+  expect(got).toBe(expected);
+});
+
+test("Testing contains numbers -- success", () => {
+  const expected = true;
+  const got = mut.containsNumbers("hello123");
+  expect(got).toBe(expected);
+});
+
+test("Testing contains number -- success", () => {
+  const expected = false;
+  const got = mut.containsNumbers("hello");
+  expect(got).toBe(expected);
+});
+
+test("Testing contains numbers -- success", () => {
+  const expected = true;
+  const got = mut.containsNumbers(123);
+  expect(got).toBe(expected);
+});
